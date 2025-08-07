adf9c2fc778f789e992638ded093ba9e6997bcb9
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-08-07 22:19:04 +0600

    project intial setup done

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="container mx-auto px-4 text-justify">
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi inventore
+        necessitatibus vitae expedita sit quo dolore iste nulla aut deserunt
+        similique temporibus repudiandae natus, error suscipit earum facere
+        
+      </h1>
+    </div>
+  );
+};
+
+export default About;
